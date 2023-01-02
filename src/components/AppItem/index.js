842c3bc94661c86d6afc77e35f18bc7c4bf84328
saierukala <sai.erukala1508@gmail.com>
2023-01-02 07:55:04 +0000

    App Store

---
+++ src/components/AppItem/index.js
@@ -0,0 +1,17 @@
+// Write your code here
+// Write your code here
+import './index.css'
+
+const AppItem = props => {
+  const {appDetails} = props
+  const {appName, imageUrl} = appDetails
+
+  return (
+    <li className="app-container">
+      <img src={imageUrl} className="app-image" alt={appName} />
+      <p className="app-name">{appName}</p>
+    </li>
+  )
+}
+
+export default AppItem
---
+++ src/components/TabItem/index.js
@@ -0,0 +1,28 @@
+// Write your code here
+// Write your code here
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, onChangeTab, isActive} = props
+  const {displayText, tabId} = tabDetails
+
+  const activatedClass = tabId === isActive ? 'active-tab' : ''
+
+  const onTabChange = () => {
+    onChangeTab(tabId)
+  }
+
+  return (
+    <li>
+      <button
+        type="button"
+        className={`tab-item ${activatedClass}`}
+        onClick={onTabChange}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
